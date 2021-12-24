4dcf0643f171e3e38acf8438aa96d5c74c573f1f
Jameson Chen <[jamesonychan@gmail.com]>
2021-12-24 07:13:51 -0500

    finished styling sidebar both phone screen and desktop screen. starting on pins page fetching query and styling.

---
+++ src/components/CreatePin.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function CreatePin() {
+    return (
+        <div>
+            create pin
+        </div>
+    )
+}
+
+export default CreatePin
---
+++ src/components/MasonryLayout.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function MasonryLayout() {
+    return (
+        <div>
+            MasonryLayout
+        </div>
+    )
+}
+
+export default MasonryLayout
