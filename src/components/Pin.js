b33ab192ea1c593a772c6ff4c3f396576afd0ab7
Jameson Chen <[jamesonychan@gmail.com]>
2021-12-26 01:53:55 -0500

    add optional chaining on user.googleId to prevent when no one is logged in

--- src/components/Pin.js
+++ src/components/Pin.js
@@ -14,7 +14,7 @@ function Pin({ pin: { postedBy, image, _id, destination, save } }) {
   const navigate = useNavigate();
 
   const alreadySaved = !!save?.filter(
-    (item) => item.postedBy._id === user.googleId
+    (item) => item.postedBy._id === user?.googleId
   )?.length;
 
   const savePin = (id) => {
@@ -25,10 +25,10 @@ function Pin({ pin: { postedBy, image, _id, destination, save } }) {
         .insert("after", "save[-1]", [
           {
             _key: uuidv4(),
-            userId: user.googleId,
+            userId: user?.googleId,
             postedBy: {
               _type: "postedBy",
-              _ref: user.googleId,
+              _ref: user?.googleId,
             },
           },
         ])
@@ -103,7 +103,7 @@ function Pin({ pin: { postedBy, image, _id, destination, save } }) {
                   {destination.length > 15 ? `${destination.slice(0,15)}...` : destination}
                 </a>
               )}
-              {postedBy?._id === user.googleId && (
+              {postedBy?._id === user?.googleId && (
                 <button
                   type="button"
                   onClick={(e) => {
